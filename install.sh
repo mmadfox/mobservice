f59bc77abf34687c2bf8bac807551dfd2c7b92c5
Sergey Liskonog <sergey.liskonog@yandex.ru>
2015-02-22 13:25:08 +0300

    ref

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DIR=$HOME/go/src/mobservice
 echo "Install mobservice into $DIR"
-
 go get github.com/Shaked/gomobiledetect
 go get github.com/gorilla/mux
 go get github.com/pmylund/go-cache
 go build $DIR/mobservice.go
+#go install $DIR/mobservice.go
 chmod a+x $DIR/mobservice
 cp $DIR/mobservice $DIR/../../bin/mobservice
 cp $DIR/rc.d/mobservicectl $DIR/../../bin/mobservicectl
